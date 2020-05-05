a875a49d74c1e7fe8b1947346947cba8825e43ee
iruseg00 <isiruseg@gmail.com>
2020-05-05 11:13:24 +0300

    nits

--- lab4/bot/Bot.js
+++ lab4/bot/Bot.js
@@ -4,8 +4,7 @@ require("./observer.js")();
 
 var GroupsService = require("./services/GroupsService");
 var ms = require("./constants/ms3");
-var logger = require("./logs/pino")
-logger.info("message")
+
 GroupsService.create(ms);
 
 
